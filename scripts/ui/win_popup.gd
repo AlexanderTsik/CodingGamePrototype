8f5e93941d8cf02a9678d53d56a8b6141499b0e7
BQ-tyupi <beqagoma@gmail.com>
2026-07-07 02:21:54 +0400

    levels changed, still needs changing 18-20

--- scripts/ui/win_popup.gd
+++ scripts/ui/win_popup.gd
@@ -49,6 +49,7 @@ func _build() -> void:
 	_panel.offset_bottom   = 240.0
 	_panel.grow_horizontal = Control.GROW_DIRECTION_BOTH
 	_panel.grow_vertical   = Control.GROW_DIRECTION_BOTH
+	_panel.z_index = 10
 	_main.add_child(_panel)
 
 	var margin = MarginContainer.new()
